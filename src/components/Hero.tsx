1bea847fed4fb0b61a2e53b8c3162985798db003
Erkhemtugs <erhemevlee@gmail.com>
2026-05-02 19:50:20 -0500

    added chat us button

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -49,14 +49,14 @@ export const Hero = () => {
               >
                 Get Free Quote
               </Button>
-              {/* <Button
+              <Button
                 variant="outline"
                 size="lg"
                 className="border-green-600 text-green-600 hover:bg-green-50 px-8 py-4 text-lg font-semibold rounded-full transition-all duration-300"
-                onClick={() => window.open('tel:+17473556316')}
+                onClick={() => window.open('https://m.me/diana.podgornaia.2025', '_blank')}
               >
-                Book Now
-              </Button>           */}
+                Chat Us
+              </Button>
               <Button
                 variant="outline"
                 size="lg"
